38bdbc89e5c18921835e44f959975860c40a5544
jwringer <jwringer@iu.edu>
2019-12-04 17:07:12 -0500

    Power Shot complete

--- godot/scenes/PowerShot.gd
+++ godot/scenes/PowerShot.gd
@@ -1,6 +1,5 @@
-extends Control
+extends RigidBody2D
 
-var new_shot = preload("res://scenes/PowerShot.tscn")
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
@@ -8,10 +7,8 @@ var new_shot = preload("res://scenes/PowerShot.tscn")
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
-func _on_Button_pressed():
-	var shot = new_shot.instance()
-	shot.position = Vector2(400, 400)
-	add_child(shot)
+
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
